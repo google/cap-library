ad92c167a99c7ea518d2da9be6ac2ca01c0b6c30
Steve Hakusa <shakusa at google.com>
2011-02-28 20:06:19 -0500

    Validator web app, a demo application for the CAP library.

---
+++ validator/test/com/google/publicalerts/cap/validator/MapVisualizerTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.publicalerts.cap.validator;
+
+import java.util.List;
+
+import org.json.JSONArray;
+
+import junit.framework.TestCase;
+
+import com.google.common.collect.Lists;
+import com.google.publicalerts.cap.Alert;
+import com.google.publicalerts.cap.TestUtil;
+
+/**
+ * Tests for {@link MapVisualizer}.
+ *
+ * @author shakusa@google.com (Steve Hakusa)
+ */
+public class MapVisualizerTest extends TestCase {
+
+  private MapVisualizer viz;
+  private List<Alert> alerts;
+
+  public MapVisualizerTest(String name) {
+    super(name);
+  }
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    this.alerts = Lists.newArrayList();
+
+    for (int i = 0; i < 3; i++) {
+      Alert.Builder alert = TestUtil.getValidAlertBuilder();
+      alert.setIdentifier(String.valueOf(i));
+      alerts.add(alert.build());
+    }
+    this.viz = new MapVisualizer(alerts);
+  }
+
+  public void testGetAlertJs() throws Exception {
+    JSONArray alerts = viz.getAlertsJs();
+    assertEquals(3, alerts.length());
+    assertNotNull(alerts.getJSONObject(0).getJSONArray("centroid"));
+    assertNotNull(alerts.getJSONObject(0).get("infoWindowContent"));
+    assertNotNull(alerts.getJSONObject(0).getJSONArray("polygons"));
+  }
+
+  public void testGetAbbreviatedAlertJs() throws Exception {
+    JSONArray alerts = viz.getAlertsJs(true);
+    assertEquals(3, alerts.length());
+    assertNotNull(alerts.getJSONObject(0).getJSONArray("centroid"));
+    assertNotNull(alerts.getJSONObject(0).get("infoWindowContent"));
+    assertFalse(alerts.getJSONObject(0).has("polygons"));
+  }
+
+  public void testGetCentroid() throws Exception {
+    JSONArray polygons = new JSONArray();
+    assertCentroid(0, 0, viz.getCentroid(polygons));
+
+    polygons.put(makePolygon(0, 0, 0, 1, 1, 1, 1, 0, 0, 0));
+    assertCentroid(0.5, 0.5, viz.getCentroid(polygons));
+
+    polygons.put(makePolygon(0, 0, 0, -1, -1, -1, -1, 0, 0, 0));
+    assertCentroid(0.0, 0.0, viz.getCentroid(polygons));
+  }
+
+  private JSONArray makePolygon(double...coords) throws Exception {
+    JSONArray polygon = new JSONArray();
+    for (int i = 0; i < coords.length; i += 2) {
+      double lat = coords[i];
+      double lng = coords[i + 1];
+      polygon.put(new JSONArray().put(lat).put(lng));
+    }
+    return polygon;
+  }
+
+  private void assertCentroid(double lat, double lng, JSONArray centroid)
+      throws Exception {
+    assertEquals(lat, centroid.getDouble(0));
+    assertEquals(lng, centroid.getDouble(1));
+  }
+}
---
+++ validator/war/map.js
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+(function() {
+
+var activeAlert;
+
+function setupMap(mapEl) {
+  if (!mapEl) {
+    return;
+  }
+  mapEl.style.width = "700px";
+  mapEl.style.height = "400px";
+  var map = new google.maps.Map(mapEl, {
+    zoom: 3,
+    center: new google.maps.LatLng(40.044437,-97.734375),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: true,
+    scaleControl: true,
+    navigationControl: true,
+    navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL}
+  });
+
+  var bounds = [90, 180, -90, -180];
+  for (var i = 0; i < alerts.length; i++) {
+    var alert = alerts[i];
+    if (alert.polygons) {
+      alert.gmPolygons = [];
+      for (var j = 0; j < alert.polygons.length; j++) {
+        var polygon = alert.polygons[j];
+        var path = [];
+        for (var k = 0; k < polygon.length; k++) {
+          var point = polygon[k];
+          checkBounds(bounds, point);
+          path.push(new google.maps.LatLng(point[0], point[1]));
+        }
+        if (path.length > 0) {
+          alert.gmPolygons.push(
+              new google.maps.Polygon({
+                  path: path,
+                  fillColor: "#FF0000",
+                  fillOpacity: 0.1,
+                  strokeColor: "#FF0000",
+                  strokeOpacity: 1.0,
+                  strokeWeight: 2,
+                  map: map
+              }));
+        }
+      }
+    }
+    if (alert.centroid) {
+      checkBounds(bounds, alert.centroid);
+      var position = new google.maps.LatLng(alert.centroid[0], alert.centroid[1]);
+      alert.gmMarker = new google.maps.Marker({
+        position: position,
+        map: map
+      });
+      if (alert.infoWindowContent) {
+        google.maps.event.addListener(alert.gmMarker, 'click',
+            open(map, alert.gmMarker, alert));
+      }      
+    }
+  }
+
+  if (bounds[0] != 90) {
+    map.fitBounds(new google.maps.LatLngBounds(
+        new google.maps.LatLng(bounds[0], bounds[1]),
+        new google.maps.LatLng(bounds[2], bounds[3])));
+  }
+  
+  if (alerts.length == 1 && alerts[0].gmMarker) {
+    window.setTimeout(open(map, alerts[0].gmMarker, alerts[0]), 500);
+  }
+}
+
+function checkBounds(bounds, point) {
+  bounds[0] = Math.min(bounds[0], point[0]);
+  bounds[1] = Math.min(bounds[1], point[1]);
+  bounds[2] = Math.max(bounds[2], point[0]);
+  bounds[3] = Math.max(bounds[3], point[1]);
+}
+
+function open(map, marker, alert) {
+  return function () {
+    if (activeAlert) {
+      if (activeAlert.infoWindow) {
+        activeAlert.infoWindow.close();
+        activeAlert.infoWindow = null;
+      }
+      if (activeAlert.gmPolygons) {
+        for (var i = 0; i < activeAlert.gmPolygons.length; i++) {
+          activeAlert.gmPolygons[i].setOptions({
+              fillOpacity: 0.1,
+              fillColor: "#FF0000",
+              strokeColor: "#FF0000",
+              zindex: 100
+          });
+        }
+      }
+      if (activeAlert.gmMarker) {
+        activeAlert.gmMarker.setOptions({
+            icon: 'http://www.google.com/intl/en_us/mapfiles/ms/micons/red-dot.png',
+            zIndex: 100
+        });
+      }
+    }
+    activeAlert = alert;
+    if (activeAlert.gmPolygons) {
+      for (var i = 0; i < activeAlert.gmPolygons.length; i++) {
+        activeAlert.gmPolygons[i].setOptions({
+          fillOpacity: 0.2,
+          fillColor: "#0000FF",
+          strokeColor: "#0000FF",
+          zindex: 200
+        });
+      }
+    }
+    if (activeAlert.gmMarker) {
+      activeAlert.gmMarker.setOptions({
+          icon: 'http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png',
+          zIndex: 200
+      });
+    }
+    if (activeAlert.infoWindowContent) {
+      activeAlert.infoWindow = new google.maps.InfoWindow({
+          content: activeAlert.infoWindowContent
+      });
+      activeAlert.infoWindow.open(map, marker);
+    }
+  };
+}
+
+function onload() {
+  setupMap(document.getElementById('map'));
+}
+document.body.onload = onload;
+
+})();
